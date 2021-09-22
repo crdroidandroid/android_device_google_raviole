386d4b5c6a5fda5678b5a4316fd8c3ed31e87d0c
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:11:38 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 7c6f7dd7ba4157308b958f37bcb4eea008051590)

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := lineage_oriole
 PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=oriole \
+    PRIVATE_BUILD_DESC="oriole-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+
+BUILD_FINGERPRINT := google/oriole/oriole:12/SQ1D.211205.016.A1/7957957:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/oriole/oriole-vendor.mk)
