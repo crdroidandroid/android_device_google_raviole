8e98eff50d9f9972fffe82a27671fe1e8d280850
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:11:38 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 7c6f7dd7ba4157308b958f37bcb4eea008051590)

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := lineage_raven
 PRODUCT_MODEL := Pixel 6 Pro
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=raven \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7888514 release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.016.A1/7888514:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
