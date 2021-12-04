1097c281239daf786141306efbf83bc4668ba442
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:07:40 +0530

    Add device manifest for radio
    Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,3 +2,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-lineage
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
+# Radio
+DEVICE_MANIFEST_FILE += \
+    device/google/raviole/manifest_radio.xml
