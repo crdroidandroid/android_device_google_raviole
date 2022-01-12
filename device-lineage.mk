e659b38b3a936a7620ff2f4f32292da9c61968b6
Matt Filetto <matt.filetto@gmail.com>
2022-01-12 06:09:38 -0800

    raviole: disable rescue
    * Something funky happens during boot on raven and SystemUI crashes a bunch
      before eventually succeeding . I think this has something to do with the
      Pixel udfps code yanked from ProtonAOSP. Disabling rescue mode for now as
      sometimes it gets triggered during the above mentioned sequence.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ieffb1e64711e639b96eb5ad75593a7792918edcf
    
    # Conflicts:
    #	device-lineage.mk
    
    # Conflicts:
    #	device-lineage.mk

--- device-lineage.mk
+++ device-lineage.mk
@@ -74,5 +74,10 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
 
 # Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.disable_rescue=true
+
+# Vendor Properties
 TARGET_VENDOR_PROP := device/google/raviole/vendor.prop
+
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
