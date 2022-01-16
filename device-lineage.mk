3096dfeb30595619d70cff085b88cd111887dc9c
Matt Filetto <matt.filetto@gmail.com>
2022-01-16 07:02:27 -0800

    raviole: device uses prebuilt kernel
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: If866bfd6dbc5ba4d2b5a6b5dfcc7eb5fa3d5b34b

--- device-lineage.mk
+++ device-lineage.mk
@@ -3,3 +3,5 @@ DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-lineage
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
