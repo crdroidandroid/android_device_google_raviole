7945284eb6eab9645fa74eb6b70e54eb734570d4
Matt Filetto <matt.filetto@gmail.com>
2022-01-16 06:57:18 -0800

    raviole: add initial Lineage makefiles
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I59acfda996fdb93ad6ceb6c2eba713cc89e886b1

---
+++ lineage_oriole.mk
@@ -0,0 +1,11 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/raviole/device-lineage.mk)
+$(call inherit-product, device/google/raviole/aosp_oriole.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_oriole
+PRODUCT_MODEL := Pixel 6
+PRODUCT_RESTRICT_VENDOR_FILES := false
