78e291595d1d074626ea15e759e42f32dd3923ff
Matt Filetto <matt.filetto@gmail.com>
2022-01-16 07:41:40 -0800

    raven: update build fingerprint to Jan 2022 Release
    * raven:12/SQ1D.220105.007/8030436:user/release-keys
    * Also updated all proprietary files to match
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I63cc92b3e1a9677df2a5ae7b8641133cf6f67c7f
    
    # Conflicts:
    #	lineage_raven.mk

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -16,8 +16,8 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=raven \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.017 7955197 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.017/7955197:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
