fe4579c7860baa9e166a384b52bed11e23743d69
Matt Filetto <matt.filetto@gmail.com>
2022-01-24 02:47:49 -0800

    raviole: use extract-files helper scripts from StatixOS instead
    * to keep things simple
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ie0050787a7c95176768d32b3c80c0950dc0887f7

--- oriole/extract-files.sh
+++ oriole/extract-files.sh
@@ -57,6 +57,5 @@ fi
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
