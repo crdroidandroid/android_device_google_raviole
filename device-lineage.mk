2d9f35d3d0ef60327a4316873e4cee8fe84c773d
Matt Filetto <matt.filetto@gmail.com>
2022-01-24 02:37:55 -0800

    raviole: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097
    
    # Conflicts:
    #	device-lineage.mk

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,1 +1,5 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-lineage
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/raviole/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
