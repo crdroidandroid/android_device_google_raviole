8e6882923d6497f6224c41c76c9298ec802abb58
Matt Filetto <matt.filetto@gmail.com>
2022-01-25 06:51:41 -0800

    raviole: build all versions of hardware packages
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,22 +14,28 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
     hardware.google.bluetooth.ccc@1.0.vendor \
+    hardware.google.bluetooth.sar@1.0.vendor \
     hardware.google.bluetooth.sar@1.1.vendor
 
 # Codec2
 PRODUCT_PACKAGES += \
-    libavservices_minijail_vendor:64 \
+    android.hardware.media.c2@1.0.vendor \
+    libavservices_minijail.vendor \
+    libavservices_minijail_vendor:32 \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
-    libmedia_ecoservice.vendor
+    libmedia_ecoservice.vendor \
+    libstagefright_bufferpool@2.0.1.vendor
 
 # Confirmation UI
 PRODUCT_PACKAGES += \
-    android.hardware.confirmationui@1.0.vendor \
-    android.hardware.confirmationui@1.0-lib.trusty
+    android.hardware.confirmationui@1.0-lib.trusty:64 \
+    android.hardware.confirmationui@1.0.vendor:64 \
+    libteeui_hal_support.vendor:64
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -37,8 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Identity credential
 PRODUCT_PACKAGES += \
-    android.hardware.identity_credential.xml \
-    android.hardware.identity-support-lib.vendor:64
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
 
 # Nos
 PRODUCT_PACKAGES += \
@@ -70,48 +76,69 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libwifi-hal:64
 
-# Misc Interfaces
+# Misc interfaces
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor \
-    android.hardware.biometrics.fingerprint-V1-ndk_platform.vendor \
-    android.hardware.drm@1.4.vendor \
-    android.hardware.keymaster@4.1.vendor \
-    android.hardware.oemlock@1.0.vendor \
+    android.hardware.biometrics.common-V1-ndk_platform.vendor:64 \
+    android.hardware.biometrics.fingerprint-V1-ndk_platform.vendor:64 \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.input.classifier@1.0.vendor:64 \
+    android.hardware.input.common@1.0.vendor:64 \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
     android.hardware.neuralnetworks-V1-ndk_platform.vendor \
-    android.hardware.neuralnetworks@1.3.vendor \
-    android.hardware.power@1.2.vendor \
-    android.hardware.radio@1.6.vendor \
+    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.power@1.0.vendor:64 \
+    android.hardware.power@1.1.vendor:64 \
+    android.hardware.power@1.2.vendor:64 \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
-    android.hardware.tetheroffload.config@1.0.vendor \
-    android.hardware.tetheroffload.control@1.1.vendor \
-    android.hardware.weaver@1.0.vendor \
-    android.hardware.wifi@1.5.vendor
+    android.hardware.tetheroffload.config@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.1.vendor:64 \
+    android.hardware.weaver@1.0.vendor:64 \
+    android.hardware.wifi@1.1.vendor:64 \
+    android.hardware.wifi@1.2.vendor:64 \
+    android.hardware.wifi@1.3.vendor:64 \
+    android.hardware.wifi@1.4.vendor:64 \
+    android.hardware.wifi@1.5.vendor:64
 
-# Misc Interfaces
+# Misc
 PRODUCT_PACKAGES += \
-    com.google.hardware.pixel.display-V3-ndk_platform.vendor \
-    pixelpowerstats_provider_aidl_interface-cpp.vendor \
-    libaudioroutev2.vendor \
-    libcppbor.vendor \
     libGralloc4Wrapper \
-    libexynosv4l2 \
+    libaudioroutev2.vendor \
     libexynosutils \
-    libkeymaster4support.vendor \
-    libnetfilter_conntrack:64 \
-    libnfnetlink:64 \
+    libexynosv4l2 \
+    libhidltransport.vendor \
     libsfplugin_ccodec_utils.vendor \
-    libtrusty_metrics
+    libavservices_minijail_vendor \
+    libcppbor.vendor \
+    libkeymaster4support.vendor \
+    libnetfilter_conntrack \
+    libnfnetlink \
+    libtrusty_metrics \
+    pixelpowerstats_provider_aidl_interface-cpp.vendor \
+    com.google.hardware.pixel.display-V3-ndk_platform.vendor
 
+# Telephony
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.singlereg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.singlereg.xml
 
-# Telephony
 PRODUCT_PACKAGES += \
     ImsServiceEntitlement \
     Iwlan
