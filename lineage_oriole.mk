4699cfae164b4c758469e26e95737bd37e19458c
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-09-08 22:55:27 -0230

    oriole: crDroid bringup

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -6,7 +6,11 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Gapps
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole.mk)
@@ -18,6 +22,23 @@ include device/google/raviole/oriole/device-lineage.mk
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6
 PRODUCT_NAME := lineage_oriole
+
+# crDroid specific build flags
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_6 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
